a0436ab017c7585592aac4e058058050fd1d6ad7
Tom Shamp <T.shamp7@gmail.com>
2020-07-03 10:17:47 -0700

    implement sign in, sign, out and new user sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    user = User.find_by_credentials(params[:username], params[:password])
     if user.nil?
       flash.now[:errors] = ["Incorrect username and/or password"]
       render :new
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    logout_user!
+    render :new
   end
 end
